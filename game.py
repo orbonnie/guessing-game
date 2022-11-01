6c6c72ea57d330a241da47638089d7d1ad7be9c2
bonnie <arlolocos@gmail.com>
2022-11-01 13:14:42 -0400

    Updated with input validation

--- game.py
+++ game.py
@@ -12,11 +12,20 @@ random_num = random.randint(1, 100)
 guesses = 0
 
 while guess != random_num:
-  guess = int(input('Please guess a number between 1-100 '))
-  guesses += 1
-  if guess == random_num:
-    print(f'You guessed the number! It took you {guesses} tries.')
-  elif guess < random_num:
-    print(f'{guess} is too low. ')
-  elif guess > random_num:
-    print(f'{guess} is too high. ')
+  while True:
+    try:
+      guess = int(input('Please guess a number between 1-100 '))
+      break
+    except ValueError:
+      print('Please choose a NUMBER')
+
+  if 0 < guess < 101:
+    guesses += 1
+    if guess == random_num:
+      print(f'You guessed the number! It took you {guesses} tries.')
+    elif guess < random_num:
+      print(f'{guess} is too low. ')
+    elif guess > random_num:
+      print(f'{guess} is too high. ')
+  else:
+    print('Please choose a number between ** 1 and 100 **')
